50a8c0b368cd3e43152ee9777a38f943a0bf9eeb
nottdev <siriphonnot@gmail.com>
2021-06-05 18:31:42 +0700

    Change option for puppeteer launcher

--- helpers/puppeteer.helper.js
+++ helpers/puppeteer.helper.js
@@ -3,37 +3,33 @@
   Author: NottDev
   Created Date: 03/06/21 23:23
 */
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 module.exports = {
   async getContent({
-    linkUrl = '',
-    mainSelector = '',
-    containerSelector = '',
+    linkUrl = "",
+    mainSelector = "",
+    containerSelector = "",
     callback = Function,
     timeout = 30000,
   } = {}) {
     try {
-      const browser = await puppeteer.launch()
-      // const browser = await puppeteer.launch({
-      //   args: [
-      //     '--no-sandbox',
-      //     '--disable-setuid-sandbox'
-      //   ]
-      // });
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
       const page = await browser.newPage();
-       // Configure the navigation timeout
+      // Configure the navigation timeout
       await page.setDefaultNavigationTimeout(0);
       await page.goto(linkUrl, {
-        waitUntil: 'networkidle0',
+        waitUntil: "networkidle0",
       });
 
       await page.waitForSelector(mainSelector.trim(), { timeout });
-      const contentList = await page.$$eval(containerSelector.trim(), callback)
+      const contentList = await page.$$eval(containerSelector.trim(), callback);
       await browser.close();
       return contentList;
     } catch (error) {
-      console.log('[puppeteer error]: ', error);
-      return error
+      console.log("[puppeteer error]: ", error);
+      return error;
     }
-  }
-}
+  },
+};
