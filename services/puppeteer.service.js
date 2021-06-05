4154ac3e811086b5bb5d499169f7d3a2bb8f938a
nottdark <nottdark.dev@gmail.com>
2021-06-06 02:49:06 +0700

    Remive optional chaining

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -11,19 +11,19 @@ const methods = {
             containerSelector: '.cmc-homepage',
             itemSelectorAll: 'table.cmc-table > tbody > tr',
             callback: (list) => {
-              return  list.map(ele => {
+              return  list.slice(0, 10).map(ele => {
                 const price24HSign = ele.querySelector('td:nth-child(5) span span.icon-Caret-down') ? '-' : ''
                 const price7DSign = ele.querySelector('td:nth-child(6) span span.icon-Caret-down') ? '-' : ''
                 return {
-                  coinLogoUrl: ele.querySelector('td:nth-child(3) img.coin-logo')?.src,
-                  coinName: ele.querySelector('td:nth-child(3) div > a > div > div > a')?.innerText,
-                  coinSymbol: ele.querySelector('td:nth-child(3) p.coin-item-symbol')?.innerText,
-                  coinPrice: ele.querySelector('td:nth-child(4) a.cmc-link')?.innerText,
-                  price24H: price24HSign + ele.querySelector('td:nth-child(5) span')?.innerText,
-                  price7D: price7DSign +  ele.querySelector('td:nth-child(6) span')?.innerText,
-                  marketCap: ele.querySelector('td:nth-child(7) p > span:nth-child(2)')?.innerText,
-                  volume24H: ele.querySelector('td:nth-child(8) div > a > p')?.innerText,
-                  circulatingSupply: ele.querySelector('td:nth-child(9) div > div > p')?.innerText,
+                  coinLogoUrl: (ele.querySelector('td:nth-child(3) img.coin-logo') || {}).src,
+                  coinName: (ele.querySelector('td:nth-child(3) div > a > div > div > a') || {}).innerText,
+                  coinSymbol: (ele.querySelector('td:nth-child(3) p.coin-item-symbol') || {}).innerText,
+                  coinPrice: (ele.querySelector('td:nth-child(4) a.cmc-link') || {}).innerText,
+                  price24H: (price24HSign + ele.querySelector('td:nth-child(5) span') || {}).innerText,
+                  price7D: (price7DSign +  ele.querySelector('td:nth-child(6) span') || {}).innerText,
+                  marketCap: (ele.querySelector('td:nth-child(7) p > span:nth-child(2)') || {}).innerText,
+                  volume24H: (ele.querySelector('td:nth-child(8) div > a > p') || {}).innerText,
+                  circulatingSupply: (ele.querySelector('td:nth-child(9) div > div > p') || {}).innerText,
                 }
               })
             }
@@ -35,7 +35,7 @@ const methods = {
               name: 'Top 10 Cryptocurrencies',
               referenceFrom: 'https://coinmarketcap.com/',
               language: lang,
-              items: Array.isArray(result) ? result.slice(0, 10) : []
+              items: result
             })
           }
         } catch (error) {
